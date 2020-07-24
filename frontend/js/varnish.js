9f6d96fe47cabdfacfa619658bcdf9806db04ec2
mika <mirouf83@live.fr>
2020-07-24 21:04:25 +0200

    verif

--- frontend/js/varnish.js
+++ frontend/js/varnish.js
@@ -1,12 +1,8 @@
 // fonction pour la création du vernis dans le localStorage
 const items = ()=>{
 
-
   let items = [];
   let woods = JSON.stringify(items);
-
-
-
 
   localStorage.setItem ("woods", woods);
 
